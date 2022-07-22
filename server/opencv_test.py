6509fab00c2e92d819972cb07fc08cfcb2254a2f
DoyunShin <doyun.shin@gmail.com>
2022-07-22 13:42:27 +0900

    Update opencv_test.py

--- server/opencv_test.py
+++ server/opencv_test.py
@@ -4,6 +4,8 @@ vcap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = vcap.read()
+    if not ret:
+        continue
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
