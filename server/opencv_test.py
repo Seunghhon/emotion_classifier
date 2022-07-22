35a101a659ba60fd6d96a303653efbf463a03b71
DoyunShin <doyun.shin@gmail.com>
2022-07-22 13:51:48 +0900

    Update opencv_test.py

--- server/opencv_test.py
+++ server/opencv_test.py
@@ -1,6 +1,35 @@
 import cv2
 import os
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    rawCapture.truncate(0)
+
+    if key == ord("q"):
+        break
+
+
+exit()
+
+
+
+
+
+
+
+
+
+#############################
 os.system("sudo modprobe bcm2835-v4l2")
 vcap = cv2.VideoCapture(0)
 
