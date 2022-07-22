e327f313e8cf1c2fc0be532a2e157931213a901e
Seunghoon <89653142+Seunghoon0326@users.noreply.github.com>
2022-07-22 11:20:23 +0900

    Update app.py

--- server/app.py
+++ server/app.py
@@ -5,7 +5,6 @@ class maneger():
     def __init__(self):
         from PIL import Image, ImageFont, ImageDraw
         import os
-        import argparse
         import cv2
         import numpy as np
         import torch
@@ -15,12 +14,12 @@ class maneger():
         import models
         import json
         import random
+        import time
 
         self.Image = Image
         self.ImageFont = ImageFont
         self.ImageDraw = ImageDraw
         self.os = os
-        self.argparse = argparse
         self.cv2 = cv2
         self.np = np
         self.torch = torch
@@ -30,11 +29,16 @@ class maneger():
         self.models = models
         self.json = json
         self.random = random
+        self.time = time
 
         self.lb = ""
+        self.lb_status = False
+        self.tm = 0
+        self.lt =  ""
+        self.pin = False
         self.class_labels = ['기쁨', '당황', '분노', '불안', '상처', '슬픔', '중립']
-        self.class_labels_dict = {'기쁨': 0, '당황': 1, '분노': 2,
-                     '불안': 3, '상처': 4, '슬픔': 5, '중립': 6}
+        self.class_labels_dict = {'기쁨': 0, '당황': 1, '분노': 2, '불안': 3, '상처': 4, '슬픔': 5, '중립': 6}
+        self.count = {"기쁨": 0, "당황": 0, "분노": 0, "불안": 0, "상처": 0, "슬픔": 0, "중립": 0}
         self.face_classifier = cv2.CascadeClassifier('face_classifier.xml')
         self.display_color = (246, 189, 86)
 
@@ -47,7 +51,7 @@ class maneger():
 
     def get_letter(self):
         if self.lb == "":
-            return " "
+            return ""
         
         return self.letter[self.lb][self.random.randint(0, len(self.letter[self.lb])-1)]
 
@@ -59,12 +63,43 @@ class maneger():
         cap = self.cv2.VideoCapture(0)
 
         while True:
+            print("COUNT:", self.count, "TM: ", self.tm, "LT: ", self.lt, "LB: ", self.lb, "PIN: ", self.pin, "LB_STATUS: ", self.lb_status)
+            if self.lb_status == False:
+                self.count = {"기쁨": 0, "당황": 0, "분노": 0, "불안": 0, "상처": 0, "슬픔": 0, "중립": 0}
+                self.tm = 0
+                self.lt =  ""
+                self.pin = False
+
+            elif self.pin == False:
+                #check the time is after the two seconds
+                if self.time.time() - self.tm > 2:
+                    print("TIMEOUT 2SEC")
+                    self.tm = self.time.time()
+                    #check largest value in the count
+                    max_value = 0
+                    for key in self.count:
+                        if self.count[key] > max_value:
+                            max_value = self.count[key]
+                            self.lb = key
+                    self.lt = self.get_letter()
+                    self.pin = True
+
+            else:
+                if self.time.time() - self.tm > 10:
+                    print("TIMEOUT 10SEC")
+                    self.tm = 0
+                    self.lt = ""
+                    self.pin = False
+                    self.lb_status = False
+                    self.count = {"기쁨": 0, "당황": 0, "분노": 0, "불안": 0, "상처": 0, "슬픔": 0, "중립": 0}
+
+
             ret, frame = cap.read()
-            self.lb_status = False
             frame = self.cv2.flip(frame, 1)
             gray = self.cv2.cvtColor(frame, self.cv2.COLOR_BGR2GRAY)
             faces = self.face_classifier.detectMultiScale(gray, 1.3, 5)
-
+            if faces == ():
+                self.lb_status = False
             for (x, y, w, h) in faces:
                 self.cv2.rectangle(frame, (x, y), (x+w, y+h), self.display_color, 2)
                 roi_gray = gray[y:y+h, x:x+w]
@@ -81,9 +116,12 @@ class maneger():
                     probs = self.torch.exp(tensor).detach().numpy()
                     prob = self.np.max(probs) * 100
                     pred = self.torch.max(tensor, dim=1)[1].tolist()
-                    
-                    self.lb = self.class_labels[pred[0]]
+                    #append the prediction to the count
+                    self.count[self.class_labels[pred[0]]] += 1
+                    if self.lb_status == False:
+                        self.tm = self.time.time()
                     self.lb_status = True
+
             
             if self.lb_status == False:
                 self.lb = ""
@@ -124,7 +162,7 @@ m = maneger()
 def root():
     rtn = {}
     rtn.update({"emotion": m.lb})
-    rtn.update({"letter": m.get_letter()})
+    rtn.update({"letter": m.lt})
     # dump with utf-8
     return json.dumps(rtn, ensure_ascii=False)
 
