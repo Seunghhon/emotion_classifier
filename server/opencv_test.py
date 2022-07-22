ed066227e315c9d5d720bf28caddfe4744a974d0
DoyunShin <doyun.shin@gmail.com>
2022-07-22 13:42:38 +0900

    Update opencv_test.py

--- server/opencv_test.py
+++ server/opencv_test.py
@@ -5,6 +5,7 @@ vcap = cv2.VideoCapture(0)
 while True:
     ret, frame = vcap.read()
     if not ret:
+        print("Frame passed")
         continue
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
